ded903142d7b21d9dde0923ea2037bd88d7daff7
frustratedswede <andrew.mossefin@gmail.com>
2017-05-05 05:39:19 -0600

    add invocies find controller
    passing all spec harness tests

---
+++ app/controllers/api/v1/invoices_find_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::InvoicesFindController < ApplicationController
+
+  def index
+    invoices = Invoice.where(invoice_params)
+    render json: invoices
+  end
+
+  def show
+    invoice = Invoice.find_by(invoice_params)
+    render json: invoice
+  end
+
+  private
+
+  def invoice_params
+    params.permit(:id, :customer_id, :merchant_id, :status, :created_at, :updated_at)
+  end
+end
